1ed663c0bea3752870f4d11c5a8c4af3ed64e747
iessel-git <isaac@csetechsolution.com>
2025-11-16 02:42:37 -0500

    Update Category.java

--- src/main/java/com/cse/financeapp/models/Category.java
+++ src/main/java/com/cse/financeapp/models/Category.java
@@ -5,19 +5,13 @@ public class Category {
     private String name;
     private String description;
 
-    public Category() {}
-
     public Category(int id, String name, String description) {
         this.id = id;
         this.name = name;
         this.description = description;
     }
 
-    public Category(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
-
+    // Getters and setters
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
 
