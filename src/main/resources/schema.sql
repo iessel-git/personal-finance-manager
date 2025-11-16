ed374b4da40284ff6053a6b09591e9871b2c649e
iessel-git <isaac@csetechsolution.com>
2025-11-15 19:12:35 -0500

    Modify expenses table structure
    Removed category_id and added description column to expenses table.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,12 +12,12 @@ CREATE TABLE IF NOT EXISTS categories (
 -- ==============================
 CREATE TABLE IF NOT EXISTS expenses (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    description TEXT NOT NULL,
+    category TEXT NOT NULL,
     amount REAL NOT NULL,
     date TEXT NOT NULL,
-    category_id INTEGER,
-    FOREIGN KEY (category_id) REFERENCES categories(id)
+    description TEXT
 );
+
 
 -- ==============================
 --  Budgets Table
